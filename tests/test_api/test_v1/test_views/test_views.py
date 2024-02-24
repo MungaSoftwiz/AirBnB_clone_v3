1ad67a3deb1222ccb714d72cd963819915b7012b
MungaSoftwiz <mwachilumobm@gmail.com>
2024-02-24 13:40:29 +0300

    test: api status

--- tests/test_api/test_v1/test_views/test_views.py
+++ tests/test_api/test_v1/test_views/test_views.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" """
+""" Tests json responses for the APIs"""
 import unittest
 from api.v1.app import app
 
 
 class TestStatus(unittest.TestCase):
-    """ """
+    """ Tests status behavious of the api """
     def setUp(self):
+        """ Sets up an application context """
         self.ctx = app.app_context()
         self.ctx.push()
         self.client = app.test_client()
 
     def tearDown(self):
-        """ """
+        """ Tears down an application context """
         self.ctx.pop()
 
     def test_status_if_ok(self):
-        """ """
+        """ Test if API returns correct status"""
         with app.test_client() as client:
             response = client.get("/api/v1/status")
             data = response.get_json()
