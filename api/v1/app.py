ef831cbc8b1edd98237ac6929d1019e74f659f43
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-02-25 10:27:38 +0300

    feat: HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """ Sets up the flask application with configurations """
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -17,6 +19,7 @@ def teardown(ctx):
 
 @app.errorhandler(404)
 def not_found(error):
+    """Handler for 404 errors """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
