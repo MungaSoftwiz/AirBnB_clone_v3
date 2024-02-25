fa96724fce77d758d73f1c5d1b7e09175c5efa7c
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-02-25 10:13:41 +0300

    feat: not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ Sets up the flask application with configurations """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 import os
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
@@ -14,6 +13,11 @@ app.register_blueprint(app_views)
 def teardown(ctx):
     """ Closes the current storage connection """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
