c5892a4cd48367c0e193574242c84c8a569d54ff
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-02-25 10:35:29 +0300

    fix: HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Sets up the flask application with configurations """
+import os
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-from models import storage
-import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
